50fd5b708cf74581fd149a9111507735866b1900
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-03-05 17:12:14 -0600

    update questions js code

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -2,22 +2,22 @@
 var questions = [
     {
         name: "question 1:  What is the first color of the rainbow?",
-        answer: Red,
-        choices: [Red,Yellow,Blue,Green]
+        answer: "Red",
+        choices: ["Red","Yellow","Blue","Green"],
     },
     {
         name: "question 2: What is the first letter of the alphabet?",
-        answer: A,
-        choices: [H, R, A, L]
+        answer: "A",
+        choices: ["H", "R", "A", "L"],
     },
     {
         name: "question 3: Who was our first president?",
-        answer: Washington,
-        choices: [Jefferson, Washington, Roosevelt, Lincoln]    
+        answer: "Washington",
+        choices: ["Jefferson", "Washington", "Roosevelt", "Lincoln"],
     },
     {
         name: "question 4: What was the first car",
-        answer: Benz,
-        choices: [Ford, Mercedes, Buick, Benz] 
+        answer: "Benz",
+        choices: ["Ford", "Mercedes", "Buick", "Benz"],
     },
-]
+];
