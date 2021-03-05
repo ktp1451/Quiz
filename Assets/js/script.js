9d7ec2773acb9730d46e2c5e6574765852652a9c
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-03-05 17:37:44 -0600

    updated java

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,11 +3,11 @@ var startBtn = document.querySelector('#start');
 //var for displaying time countdown
 var pageTime = document.querySelector('#time');
 //var for questions
-var question = document.querySelector("question");
+var question = document.querySelector("#question");
 //var for question content
-var name = document.querySelector("name");
+var name = document.querySelector("#name");
 //var for answer choices
-var choices = document.querySelector("choices");
+var choices = document.querySelector("#choices");
 //create timer var = starting time
 var timeLeft = 60;
 //create interval timer = countdown time
@@ -23,11 +23,22 @@ function startGame(){
     displayQuestion();
 }
 
-//
+//function to display and filter through questions
 function displayQuestion() {
     let currentQuestion = questions[0]; //arrays must use brackets
-
-    console.log(currentQuestion);
+    //console.log(currentQuestion);
+    //question contect function
+    name.textContent = currentQuestion.name;
+    console.log(name); //still learning how to console log. Is this right???
+    //forEach loop to recall choices var array. currentQuestion is object
+    currentQuestion.choices.forEach(function(choice) {
+        //console.log(choice);
+        //what does this do???
+        let choiceBtn = document.createElement("button");
+        choiceBtn.textContent = choice;
+        //appends to choices container in HTML
+        choices.appendChild(choiceBtn);
+    });
 }
 
 
@@ -40,7 +51,7 @@ function timeDuration(){
     //displays time countdown
     pageTime.textContent = timeLeft;
 
-    //what would this be called?
+    //what would this be called???
     if(timeLeft === 0) {
         clearInterval(timeInterval);
     }
